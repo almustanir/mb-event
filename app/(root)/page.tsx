b85d6c6be5ce437760bdb5b93678c02fade3d2e7
almustanir <olajidemubarak561@gmail.com>
2024-03-27 09:10:18 +0100

    lll

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -25,7 +25,7 @@ export default async function Home({ searchParams }: SearchParamProps) {
         <div className="wrapper grid grid-cols-1 gap-5 md:grid-cols-2 2xl:gap-0">
           <div className="flex flex-col justify-center gap-8">
             <h1 className="h1-bold">Experience Hosting, Connecting, and Celebrating Your Events with Our Platform!</h1>
-            <p className="p-regular-20 md:p-regular-24">Access invaluable tips from over 3,168 mentors within top-tier companies through our worldwide community for booking and learning.</p>
+            <p className="p-regular-20 md:p-regular-24">Access invaluable tips from over 3,168 mentors within top-tier companies through our worldwide community for booking and learning.....❤️✅</p>
             <Button size="lg" asChild className="button w-full sm:w-fit">
               <Link href="#events">
                 Explore Now
